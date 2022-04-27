608db3b9b0fb95f351bafbdda5ab54c7e06be403
ChelderStark <helderdroid@gmail.com>
2022-04-27 08:20:10 -0300

    Add Bootstrap

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 
@@ -91,5 +92,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
